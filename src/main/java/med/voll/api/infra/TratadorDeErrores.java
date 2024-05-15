ce1b5e2ff28c8c1aa9b68ec44b54257a95dee0c3
Brayanruiz32 <maverick.brm@gmail.com>
2024-05-15 15:38:19 -0500

    implementacion de nuevo proxy restcontrolleradvice

--- src/main/java/med/voll/api/infra/TratadorDeErrores.java
+++ src/main/java/med/voll/api/infra/TratadorDeErrores.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import javax.swing.text.html.parser.Entity;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,11 +15,15 @@ import jakarta.persistence.EntityNotFoundException;
 public class TratadorDeErrores {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity tratarError404(){
+    public ResponseEntity tratarError404() {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
 
+        return ResponseEntity.badRequest().body(e.getFieldErrors().stream().map(err -> err.getField()+ ": "+err.getDefaultMessage()).toList());
 
+    }
 
 }
